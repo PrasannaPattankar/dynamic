db0424cf6909c924e7da9460c6676c5b1b90e5f4
PrasannaPattankar <prasanna.pattankar@gmail.com>
2025-04-04 16:25:53 +0530

    app1 changes

--- packages/app1/jest.config.ts
+++ packages/app1/jest.config.ts
@@ -1,15 +1,15 @@
 export default {
-  displayName: 'app1', // change for app2, app1 etc.
+  displayName: 'app1',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/dashboard',
+  coverageDirectory: '../../coverage/packages/app1',
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json', // ✅ explicitly point to the correct one
-    },
-  },
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  }
 };
