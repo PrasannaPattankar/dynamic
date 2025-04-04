26ce04d82436e41fcf856b0486b41e835225ffab
PrasannaPattankar <prasanna.pattankar@gmail.com>
2025-04-04 12:01:29 +0530

    resolved error

--- packages/app1/jest.config.ts
+++ packages/app1/jest.config.ts
@@ -6,10 +6,12 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/app1',
+  coverageDirectory: '../../coverage/packages/app2',
+
+  // ✅ Add this block to resolve TS module issues
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json', // ✅ point to correct TS config
+      tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
 };
--- packages/app2/jest.config.ts
+++ packages/app2/jest.config.ts
@@ -7,4 +7,11 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/app2',
+
+  // ✅ Add this block to resolve TS module issues
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
 };
