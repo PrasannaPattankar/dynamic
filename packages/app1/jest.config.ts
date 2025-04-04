0503d9927058e2f0dac5033e49869786b80e4453
PrasannaPattankar <prasanna.pattankar@gmail.com>
2025-04-04 16:35:28 +0530

    added app1 packages

--- packages/app1/jest.config.ts
+++ packages/app1/jest.config.ts
@@ -2,14 +2,14 @@ export default {
   displayName: 'app1',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+    '^.+\\.[tj]sx?$': ['ts-jest', { useESM: true }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/app1',
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json'
-    }
-  }
+      useESM: true,
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
 };
